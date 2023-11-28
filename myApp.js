a612d7d6484fe80e6bbe4f69daf27b967ac769f9
Arekhandia Harry <harry_p30@yahoo.com>
2023-11-28 05:54:14 +0100

    created a model

--- myApp.js
+++ myApp.js
@@ -1,12 +1,23 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+// connecting to server
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-let Person;
+// Person model
+const personSchema = mongoose.Schema({
+  name:{
+    type: String,
+    required: true
+  },
+  age: Number,
+  favouriteFoods: [String]
+});
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
